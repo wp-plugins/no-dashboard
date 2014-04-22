f73fc4b1cde41b3a7f1ea23d58f448f06fc3e70e
stevenfernandez <stevenfernandez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-22 09:16:44 +0000

    Version 1.1. Minor bug fixed.
    git-svn-id: https://plugins.svn.wordpress.org/no-dashboard/trunk@899972 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ no-dashboard.php
@@ -0,0 +1,50 @@
+<?php
+    /*
+    Plugin Name: No Dashboard
+    Plugin URI: http://www.stevenfernandez.co.uk/wordpress-plugins/
+    Description: No Dashboard is a simple plugin which removes the wordpress dashboard. No settings page, just activate the plugin and the dashboard with disappear.
+    Author: Steven Fernandez
+    Version: 1.1
+    Author URI: http://www.stevenfernandez.co.uk
+
+    === RELEASE NOTES ===
+    20-04-2014 - v1.0 - first version
+    21-04-2014 - v1.1 - fixed small bug
+    */
+
+global $wp_version;
+$exit_msg = 'No Dashboard reqiures WordPress 3.0 or newer';
+if (version_compare($wp_version, '3.0', '<')){
+    exit ($exit_msg);
+}
+
+
+function no_dashboard () {
+    global $current_user, $menu, $submenu;
+    get_currentuserinfo();
+
+   // if( ! in_array( 'administrator', $current_user->roles ) ) {
+        reset( $menu );
+        $page = key( $menu );
+        while( ( __( 'Dashboard' ) != $menu[$page][0] ) && next( $menu ) ) {
+            $page = key( $menu );
+        }
+        if( __( 'Dashboard' ) == $menu[$page][0] ) {
+            unset( $menu[$page] );
+        }
+        reset($menu);
+        $page = key($menu);
+        while ( ! $current_user->has_cap( $menu[$page][1] ) && next( $menu ) ) {
+            $page = key( $menu );
+        }
+        if ( preg_match( '#wp-admin/?(index.php)?$#', $_SERVER['REQUEST_URI'] ) &&
+            ( 'index.php' != $menu[$page][2] ) ) {
+                wp_redirect( get_option( 'siteurl' ) . '/wp-admin/edit.php');
+        }
+    //}
+}
+add_action('admin_menu', 'no_dashboard');
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== No Dashboard ===
+
+Plugin Name: No Dashboard
+Contributors: stevenfernandez
+Donate link: http://stevenfernandez.co.uk/
+Tags: no dashboard, hide dashboard
+Author: Steven Fernandez
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 4.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+No Dashboard is a simple plugin which removes the wordpress dashboard. No settings page, just activate the plugin and the dashboard with disappear.
+
+
+== Installation ==
+
+1. Upload `no-dashboard` folder to the `/wp-content/plugins/` directory or upload dashboard.zip via wordpress upload button in the plugins area in the backend.
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Changelog ==
+
+= 1.0 =
+* First Version
+
+= 1.1 =
+* Fixed small bug.
